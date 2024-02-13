f10792e57bd642d96ff7cfb6106df08d4c7bd2a1
Iago Mozart <mozart.iago@gmail.com>
2024-02-13 00:44:08 -0300

    feat: Aula 3
    - Altera método para guardar dados do pedido localmente

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,6 +1,8 @@
-import { ProductProps } from '@/utils/data/products';
-import { create } from 'zustand';
 import * as cartInMemory from '@/stores/helpers/cart-in-memory';
+import { ProductProps } from '@/utils/data/products';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 export type ProductCartProps = ProductProps & {
     quantity: number
@@ -12,15 +14,19 @@ type StateProps = {
     remove: (productId: string) => void,
 }
 
-export const useCartStore = create<StateProps>((set) => ({
-    products: [],
-    add: (product: ProductProps) => set((state) => ({
-        products: cartInMemory.add(state.products, product)
-    })),
-    remove: (productId: string) =>
-    set((state) => (
-        {
-            products: cartInMemory.remove(state.products, productId),
-        }
-    )),
-}))
+export const useCartStore = create(
+    persist<StateProps>((set) => ({
+        products: [],
+        add: (product: ProductProps) => set((state) => ({
+            products: cartInMemory.add(state.products, product)
+        })),
+        remove: (productId: string) =>
+            set((state) => (
+                {
+                    products: cartInMemory.remove(state.products, productId),
+                }
+            )),
+    }), {
+        name: "nlw-expert:cart",
+        storage: createJSONStorage(() => AsyncStorage),
+    }))
